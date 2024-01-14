a89d91280d7119ca44cb50ad1a56240124f768c8
Hila Dolev <hiladolev1@gmail.com>
2024-01-14 15:32:33 +0200

    added console.log to see the inputs values + submit input

--- client/src/components/pages/Home/Form/Form.tsx
+++ client/src/components/pages/Home/Form/Form.tsx
@@ -2,15 +2,17 @@ import { useForm } from "react-hook-form";
 
 export default function Form() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = () => {};
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={onSubmit}>
       <input type="text" placeholder="Title" {...register("title")} />
       <input
         type="text"
         placeholder="Author name"
         {...register("author_name")}
       />
+      <input type="submit" value="Submit" />
     </form>
   );
 }
