3826810fe626692520b9f76168a277617954342e
Hila Dolev <hiladolev1@gmail.com>
2024-01-14 15:05:38 +0200

    books title input

--- client/src/components/pages/Home/Form/Form.tsx
+++ client/src/components/pages/Home/Form/Form.tsx
@@ -3,5 +3,9 @@ import { useForm } from "react-hook-form";
 export default function Form() {
   const { register, handleSubmit } = useForm();
   const onSubmit = () => {};
-  return <form onSubmit={handleSubmit(onSubmit)}></form>;
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Title" {...register("title")} />
+    </form>
+  );
 }
