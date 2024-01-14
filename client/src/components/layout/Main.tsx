7ae977e07cce63b00140dd0b0d30fb9cd85b811a
Hila Dolev <hiladolev1@gmail.com>
2024-01-14 14:28:45 +0200

    created a main file that controls the layout of the page with it's elements and the home component

---
+++ client/src/components/layout/Main.tsx
@@ -0,0 +1,13 @@
+import Home from "../pages/Home/Home";
+
+export default function Main() {
+  return (
+    <>
+      <header></header>
+      <main>
+        <Home />
+      </main>
+      <footer></footer>
+    </>
+  );
+}
