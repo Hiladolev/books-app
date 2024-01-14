a26c1bbdc68fbb2f49f2bbfede6738b21166c203
Hila Dolev <hiladolev1@gmail.com>
2024-01-14 18:50:56 +0200

    added books component to the home page

--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -1,4 +1,10 @@
+import BooksList from "./BooksList/BooksList";
 import Form from "./Form/Form";
 export default function Home() {
-  return <Form />;
+  return (
+    <>
+      <Form />
+      <BooksList />
+    </>
+  );
 }
