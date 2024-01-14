d4519ccf9f13bb8646eef5913d8e00477fb8c4a8
Hila Dolev <hiladolev1@gmail.com>
2024-01-14 17:53:43 +0200

    book type for typescript + variable to set the state with only the properties that are needed + rendering the books that was found by the inputs values

--- client/src/components/pages/Home/Form/Form.tsx
+++ client/src/components/pages/Home/Form/Form.tsx
@@ -2,10 +2,17 @@ import axios from "axios";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
+type Book = {
+  title: string;
+  author_name: string[];
+  cover_i: number;
+  key: string;
+};
+
 export default function Form() {
   const endpoint = "https://openlibrary.org/search.json?q=";
   const { register, handleSubmit } = useForm();
-  const [foundBooks, setFoundBooks] = useState();
+  const [foundBooks, setFoundBooks] = useState<Book[]>([]);
   const replace_space = (author: string) => {
     return author.replaceAll(" ", "+");
   };
@@ -13,24 +20,45 @@ export default function Form() {
     axios
       .get(
         endpoint +
-          `title:${data.title}+author:${replace_space(data.author_name)}
+          `title:${data.title}+author:${replace_space(
+            data.author_name
+          )}&limit=10
     `
       )
-      .then((response) => console.log(response));
+      .then((response) => {
+        const requiredProperties = response.data.docs.map(
+          ({ title, author_name, cover_i, key }: Book) => ({
+            title,
+            ...author_name,
+            cover_i,
+            key,
+          })
+        );
+        setFoundBooks(requiredProperties);
+      });
   });
 
   return (
-    <form
-      onSubmit={onSubmit}
-      style={{ display: "flex", flexDirection: "column" }}
-    >
-      <input type="text" placeholder="Title" {...register("title")} />
-      <input
-        type="text"
-        placeholder="Author name"
-        {...register("author_name")}
-      />
-      <input type="submit" value="Submit" />
-    </form>
+    <>
+      <form
+        onSubmit={onSubmit}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <input type="text" placeholder="Title" {...register("title")} />
+        <input
+          type="text"
+          placeholder="Author name"
+          {...register("author_name")}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      {foundBooks && (
+        <ul>
+          {foundBooks.map((book: Book) => (
+            <li key={book.key}>{book.title}</li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
