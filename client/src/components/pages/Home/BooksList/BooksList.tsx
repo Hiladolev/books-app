a65e0617331dca069f21289b791b31574a138a69
Hila Dolev <hiladolev1@gmail.com>
2024-01-14 19:49:09 +0200

    making a template that right now is using an array but later will use the books from the DB table

--- client/src/components/pages/Home/BooksList/BooksList.tsx
+++ client/src/components/pages/Home/BooksList/BooksList.tsx
@@ -1,13 +1,24 @@
-export default function BooksList() {
+type Book = {
+  title: string;
+  author_name: string;
+  cover_i: number;
+  key: string;
+};
+interface BooksProps {
+  savedBooks: Book[];
+}
+export default function BooksList({ savedBooks }: BooksProps) {
   return (
     <>
       {/* dont forget to add condition - if the there are saved books */}
       <h2>Saved Books:</h2>
       <ul>
-        <li>
-          <cite>book's title</cite> by book's author
-          <image>book's image</image>
-        </li>
+        {savedBooks.map((book) => (
+          <li key={book.key}>
+            <cite>{book.title}</cite> by {book.author_name}
+            <img src="{book.cover_i}" alt={book.title}></img>
+          </li>
+        ))}
       </ul>
     </>
   );
