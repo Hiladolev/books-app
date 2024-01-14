9b32c7bcf0d6712c17aae5f585e1793e7397a4e2
Hila Dolev <hiladolev1@gmail.com>
2024-01-14 15:44:05 +0200

    added css declaration for the div that wraps the whole elements

--- client/src/components/layout/Main.tsx
+++ client/src/components/layout/Main.tsx
@@ -2,12 +2,22 @@ import Home from "../pages/Home/Home";
 
 export default function Main() {
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        overflow: "hidden",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+      }}
+    >
       <header></header>
       <main>
         <Home />
       </main>
       <footer></footer>
-    </>
+    </div>
   );
 }
