cbf480b2b7eeac5c3061e17812f03372ccaac641
Hila Dolev <hiladolev1@gmail.com>
2024-01-14 15:07:04 +0200

    input for the author name

--- client/src/components/pages/Home/Form/Form.tsx
+++ client/src/components/pages/Home/Form/Form.tsx
@@ -6,6 +6,11 @@ export default function Form() {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="Title" {...register("title")} />
+      <input
+        type="text"
+        placeholder="Author name"
+        {...register("author_name")}
+      />
     </form>
   );
 }
