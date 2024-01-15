533d7220eb9011703fd5bcf0a8be9d44d93511b9
Hila Dolev <hiladolev1@gmail.com>
2024-01-15 15:38:28 +0200

    Scaffold a controller

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+727c5358952ac712a69fc75d360b69e1bed937b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a61954993931f0f33fd292ae208850d08c774bc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
