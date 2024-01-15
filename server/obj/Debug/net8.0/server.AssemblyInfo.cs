dd352efe69a90ed40a9490a23ea64e500c5ec78b
Hila Dolev <hiladolev1@gmail.com>
2024-01-15 13:05:45 +0200

    MySQL DB connection strings

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dea65eeeb9cdfefd0d4f0dfbc6fc433fef840a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbfe20348d242c150211912ccd18d5e2dcd93d57")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
