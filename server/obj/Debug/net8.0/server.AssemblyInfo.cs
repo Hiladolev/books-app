a8a65e7e78872dc8fd15442a149905d22fd7f575
Hila Dolev <hiladolev1@gmail.com>
2024-01-15 16:11:43 +0200

    can't be added without a question mark

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae90ca4981210ebad40a29e2bbd6252e35a67985")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72724af5c09a7f520035b33dfca9e532847885bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
