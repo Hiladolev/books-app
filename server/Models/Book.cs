72724af5c09a7f520035b33dfca9e532847885bd
Hila Dolev <hiladolev1@gmail.com>
2024-01-15 16:10:10 +0200

    Plain Old C# Object that represents a single book

---
+++ server/Models/Book.cs
@@ -0,0 +1,8 @@
+namespace BookApi
+
+public class Book{
+    public string Key {get;set;}
+    public string Title {get;set;}
+    public string Author {get;set;}
+    public int Cover_i {get;set;}
+}
