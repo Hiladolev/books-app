d82c1d98834f72d9b9b7922575fe28f3264b311c
Hila Dolev <hiladolev1@gmail.com>
2024-01-19 13:56:26 +0200

    made a function in the home page that will allow to lift state up from the form component(child) to the home component (parent) by callback

--- client/src/components/pages/Home/Home.tsx
+++ client/src/components/pages/Home/Home.tsx
@@ -1,54 +1,61 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import BooksList from "./BooksList/BooksList";
 import Form from "./Form/Form";
 import axios from "axios";
-export default function Home() {
-  const savedBooks = [
-    {
-      author_name: "J.R.R. Tolkien",
-      cover_i: 9255566,
-      key: "/works/OL27448W",
-      title: "The Lord of the Rings",
-    },
-    {
-      author_name: "J.R.R. Tolkien",
-      cover_i: 8167231,
-      key: "/works/OL27479W",
-      title: "The Two Towers",
-    },
-    {
-      author_name: "J.R.R. Tolkien",
-      cover_i: 8474036,
-      key: "/works/OL14933414W",
-      title: "The Fellowship of the Ring",
-    },
-    {
-      author_name: "J.R.R. Tolkien",
-      cover_i: 10523169,
-      key: "/works/OL27516W",
-      title: "The Return of the King",
-    },
-    {
-      author_name: "J.R.R. Tolkien",
-      cover_i: 8406786,
-      key: "/works/OL262758W",
-      title: "The Hobbit",
-    },
-  ];
+import Book from "../../types/Book";
+const tempBooks = [
+  {
+    author_name: "J.R.R. Tolkien",
+    cover_i: 9255566,
+    key: "/works/OL27448W",
+    title: "The Lord of the Rings",
+  },
+  {
+    author_name: "J.R.R. Tolkien",
+    cover_i: 8167231,
+    key: "/works/OL27479W",
+    title: "The Two Towers",
+  },
+  {
+    author_name: "J.R.R. Tolkien",
+    cover_i: 8474036,
+    key: "/works/OL14933414W",
+    title: "The Fellowship of the Ring",
+  },
+  {
+    author_name: "J.R.R. Tolkien",
+    cover_i: 10523169,
+    key: "/works/OL27516W",
+    title: "The Return of the King",
+  },
+  {
+    author_name: "J.R.R. Tolkien",
+    cover_i: 8406786,
+    key: "/works/OL262758W",
+    title: "The Hobbit",
+  },
+];
 
-  const fetchSavedBooks = () => {
-    axios
-      .get(`http://localhost:5173/api/savedBooks`)
-      .then((response) => console.log(response));
+export default function Home() {
+  const [savedBooks, setSavedBooks] = useState<Book[]>(tempBooks);
+
+  const handleBooksChanges = (book: Book) => {
+    //   .post("http://localhost:5173/api/book", book)
+    //   .then((response) => console.log(response));
+    setSavedBooks((current) => [...current, book]);
   };
-  useEffect(() => {
-    fetchSavedBooks();
-  }, []);
-  //add get method to receive the saved books from the db
+  // const fetchSavedBooks = () => {
+  //   axios
+  //     .get(`http://localhost:5173/api/savedBooks`)
+  //     .then((response) => setSavedBooks(response));
+  // };
+  // useEffect(() => {
+  //   fetchSavedBooks();
+  // }, []);
 
   return (
     <>
-      <Form />
+      <Form addBook={handleBooksChanges} />
       {/* dont forget to add condition - if the there are saved books */}
       <BooksList savedBooks={savedBooks} />
     </>
