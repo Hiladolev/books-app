bec4b3c0d9484893504e4897bc401dd0bb71eafc
Hila Dolev <hiladolev1@gmail.com>
2024-01-20 14:22:49 +0200

    changed the author property to author_name

--- server/Models/Book.cs
+++ server/Models/Book.cs
@@ -3,6 +3,6 @@ namespace BookApi;
 public class Book{
     public string? Key {get;set;}
     public string? Title {get;set;}
-    public string? Author {get;set;}
+    public string? Author_name {get;set;}
     public int Cover_i {get;set;}
 }
