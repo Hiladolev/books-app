8a1aa18e49708d82ff2cd68e1c992ac19da24b28
Hila Dolev <hiladolev1@gmail.com>
2024-01-28 13:36:54 +0200

    declared a default value for the property during the property declaration instead of "?"

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c768332a88fda8a0b765e4c9deeda282395a658")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf953785be011ca04f733f91496ef2b655ac2126")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
