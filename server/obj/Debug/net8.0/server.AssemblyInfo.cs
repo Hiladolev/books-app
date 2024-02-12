88fc97698f406ee381133ac4a0e07c203e7a8b86
Hila Dolev <hiladolev1@gmail.com>
2024-02-12 16:02:45 +0200

    renamed the sql dump with one less "sql" that was there by mistake

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d55a3a2029db948539a8064104a024b6356b88dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0ed116269fe0112283e3673345fbb730ea909e")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
